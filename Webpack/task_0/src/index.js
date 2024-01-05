6f89d0259caa1dfeff5a3ebb199a9e9939e59d14
timidgeek <9liveslinds@gmail.com>
2024-01-05 13:32:01 -0600

    specific syntax required for appendages

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 import $ from "jquery";
 
-p1 = "<p>Holberton Dashboard</p>";
-p2 = "<p>Dashboard data for the students</p>";
-p3 = "<p>Copyright - Holberton School</p>";
-
-$("body").append(p1, p2, p3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
