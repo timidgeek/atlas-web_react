6171d437f6cdc4525067dbfa58cf2583f9e4e38e
timidgeek <9liveslinds@gmail.com>
2024-01-08 10:34:33 -0600

    realized button functionality was nonexistent, repaired the issues by updating dashboard_main

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from "jquery";
+import _ from 'lodash';
+
+$(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+
+  function updateCounter() {
+    // add event listener: if button clicked, increment count
+    count++;
+    $("#count").text(`${count} clicks on the button`);
+    }
+  
+  // bind debounce func to click event to handle spam possibility
+  const debounced = _.debounce(updateCounter, 500);
+  $("button").on("click", debounced)
+})
---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'image-webpack-loader',
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    }
+    ]
+  }
+};
