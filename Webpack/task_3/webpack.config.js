0b83f865024cffdfad9ea53deb9d12a7411a6a39
timidgeek <9liveslinds@gmail.com>
2024-01-08 13:50:51 -0600

    updated module paths and require plugin

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,9 +7,9 @@ module.exports = {
     port: 8564
   },
   entry: {
-    header: '/header/header.js',
-    body: '/body/body.js',
-    footer: '/footer/footer.js'
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js'
   },
   output: {
     filename: '[name].bundle.js',
