c66ba48dcd3d636c726be405be00f53a6417190f
timidgeek <9liveslinds@gmail.com>
2024-01-08 10:50:47 -0600

    not super cute - not sure if the images truly matter. but code should be correct.

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import _ from 'lodash';
 
 $(document).ready(function() {
+  $('body').append('<p id="logo"></p>')
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
