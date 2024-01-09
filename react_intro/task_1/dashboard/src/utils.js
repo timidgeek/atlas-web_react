ab3b7ae820041b659e8faef4e113a0c1f0e024dd
timidgeek <9liveslinds@gmail.com>
2024-01-09 12:34:06 -0600

    utils functions for task 1

---
+++ react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,16 @@
+// return current year
+function getFullYear() {
+  return new Date().getFullYear();
+}
+
+// 
+function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return "Holberton School";
+  }
+  else {
+    return "Holberton School main dashboard";
+  }
+}
+
+export { getFullYear, getFooterCopy };
