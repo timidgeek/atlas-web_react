29a2dea2375f399106f6a058506c5430d06f799d
timidgeek <9liveslinds@gmail.com>
2024-01-09 18:20:16 -0600

    organize files!!! most fun

--- react_intro/task_5/dashboard/src/App/App.js
+++ react_intro/task_5/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
 import logo from './atlas_logo.png';
 import './App.css';
-import { getFullYear, getFooterCopy } from './utils';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
   return (
