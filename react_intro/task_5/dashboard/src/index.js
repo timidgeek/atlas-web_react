a1ce1d430868b3424e233172f099bbd30a9ed7bc
timidgeek <9liveslinds@gmail.com>
2024-01-09 18:45:31 -0600

    gracefully annihilated 8 errors

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-import Notifications from './Notifications'
+import Notifications from './Notifications/Notifications'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
