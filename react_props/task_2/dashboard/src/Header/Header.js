5b41424cceff063f66fb36f6f7241b637a22e1dd
timidgeek <9liveslinds@gmail.com>
2024-01-10 18:16:34 -0600

    updated app tests, and created task 2

---
+++ react_props/task_2/dashboard/src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/atlas_logo.png';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>School dashboard</h1>
+    </header>
+  )
+}
+
+export default Header;
