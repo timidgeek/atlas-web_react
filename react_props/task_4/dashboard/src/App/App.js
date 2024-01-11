d4b2a57d34fa35a15280d23b8d23c0e704cebac0
timidgeek <9liveslinds@gmail.com>
2024-01-10 21:55:03 -0600

    modify app test suite, all tests passing

--- react_props/task_4/dashboard/src/App/App.js
+++ react_props/task_4/dashboard/src/App/App.js
@@ -1,16 +1,22 @@
-import './App.css';
+import React, { useState } from 'react';import './App.css';
+import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
-import React from 'react';
+import CourseList from '../CourseList/CourseList';
 
-function App() {
+function App({ isLoggedIn = false }) {
+  // initialize variable & function with useState
+  // in order to manipulate the userLoggedIn state
+  const [userLoggedIn, setUserLoggedIn] = useState(isLoggedIn);
+
   return (
     <React.Fragment>
       <Notifications />
       <div className="App">
         <Header />
+        {userLoggedIn ? <CourseList /> : <Login />}
         <Login />
         <Footer />
       </div>
