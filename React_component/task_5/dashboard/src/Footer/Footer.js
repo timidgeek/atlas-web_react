6aafb7b7314e5dc2c9ed29126c5b4465b49d4d4e
timidgeek <9liveslinds@gmail.com>
2024-01-15 14:00:08 -0600

    forgot to create new dir task_5

---
+++ React_component/task_5/dashboard/src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+  )
+}
+
+export default Footer;
