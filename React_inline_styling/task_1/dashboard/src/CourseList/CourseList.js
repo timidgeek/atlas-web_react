e3bd73d60a3df965aa32bee9bd3c9ba6240d4a88
timidgeek <9liveslinds@gmail.com>
2024-01-19 14:20:24 -0600

    table looking good but had to modify CourseListRow

--- React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import CourseListRow from "./CourseListRow";
-import './CourseList.css';
 import { CourseShape } from './CourseShape';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+// NOTE - APHRODITE WON'T ALLOW ME TO ADD STYLES TO CUSTOM `CourseListRow` MODULE. 
+// ALL STYLING OF ROWS WILL HAVE TO HAPPEN IN  `CourseListRow.js` 
 
 const CourseList = ({ listCourses }) => {
   return (
-    <table id="CourseList">
+    <table className={css(styles.table)}>
       <thead>
-        <CourseListRow isHeader={true} textFirstCell="Available courses" />
-        <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
+        <CourseListRow 
+          isHeader={true} 
+          textFirstCell="Available courses"
+        />
+        <CourseListRow 
+          isHeader={true} 
+          textFirstCell="Course name" 
+          textSecondCell="Credit"
+        />
       </thead>
       <tbody>
         {listCourses.length === 0 ? (
-          <CourseListRow isHeader={false} textFirstCell="No course available yet" />
+          <CourseListRow 
+          isHeader={false} 
+          textFirstCell="No course available yet" 
+          />
         ) : (
           listCourses.map(course => (
             <CourseListRow
@@ -29,6 +42,8 @@ const CourseList = ({ listCourses }) => {
   );
 };
 
+// PROP TYPES:
+
 CourseList.propTypes = {
   listCourses: PropTypes.arrayOf(CourseShape),
 };
@@ -36,5 +51,55 @@ CourseList.propTypes = {
 CourseList.defaultProps = {
   listCourses: [],
 };
+
+// APHRODITE STYLES:
+
+const styles = StyleSheet.create ({
+
+  table: {
+    marginTop: '20px',
+    width: '100%',
+    marginBottom: '20px',
+    borderCollapse: 'collapse'
+  },
+  
+  header: {
+    padding: '12px',
+    textAlign: 'left',
+  },
+
+  headerBorder: {
+    border: '3px solid #6A7AC0',
+  },
+
+  tableBorder: {
+    border: '1px solid #6A7AC0',
+  },
+
+  centerText: {
+    textAlign: 'center',
+  },
+  
+  leftText: {
+    textAlign: 'left',
+  },
+  
+  firstRow: {
+    ':first-child': {
+      textAlign: 'center',
+    },
+  },
+
+  secondRow: {
+    ':nth-child(2)': {
+      textAlign: 'left',
+    },
+  },
+
+  cellSpacing: {
+    padding: '15px',
+  },
+});
+
 
 export default CourseList;
--- React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import './CourseListRow.css';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null }) => {
   if (isHeader) {
     return (
       <tr>
         {textSecondCell === null ? (
-          <th colSpan="2" style={{ backgroundColor: '#deb5b545'}}>{textFirstCell}</th>
+          <th colSpan="2" 
+              style={{ backgroundColor: '#deb5b545'}}  
+              className={css(styles.firstRow, styles.header, styles.headerBorder, styles.cellSpacing)}>
+                {textFirstCell}
+          </th>
         ) : (
           <>
-            <th style={{ backgroundColor: '#f5f5f5ab' }}>{textFirstCell}</th>
-            <th style={{ backgroundColor: '#f5f5f5ab' }}>{textSecondCell}</th>
+            <th style={{ backgroundColor: '#f5f5f5ab' }}
+              className={css(styles.header, styles.secondRow, styles.headerBorder, styles.cellSpacing)}>
+                {textFirstCell}
+            </th>
+            <th style={{ backgroundColor: '#f5f5f5ab' }}
+            className={css(styles.header, styles.secondRow, styles.headerBorder, styles.cellSpacing)}>
+              {textSecondCell}
+            </th>
           </>
         )}
       </tr>
@@ -19,8 +29,8 @@ const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null 
   } else {
     return (
       <tr>
-        <td>{textFirstCell}</td>
-        <td>{textSecondCell}</td>
+        <td className={css(styles.tableBorder, styles.cellSpacing)}>{textFirstCell}</td>
+        <td className={css(styles.tableBorder, styles.cellSpacing)}>{textSecondCell}</td>
       </tr>
     )
   }
@@ -34,5 +44,53 @@ CourseListRow.propTypes = {
     PropTypes.number,
   ]),
 };
+// APHRODITE STYLES:
+
+const styles = StyleSheet.create ({
+
+  table: {
+    marginTop: '20px',
+    width: '100%',
+    marginBottom: '20px',
+    borderCollapse: 'collapse'
+  },
+  
+  header: {
+    padding: '12px',
+    textAlign: 'left',
+  },
+
+  headerBorder: {
+    border: '3px solid #6A7AC0',
+  },
+
+  tableBorder: {
+    border: '1px solid #6A7AC0',
+  },
+
+  centerText: {
+    textAlign: 'center',
+  },
+  
+  leftText: {
+    textAlign: 'left',
+  },
+  
+  firstRow: {
+    ':first-child': {
+      textAlign: 'center',
+    },
+  },
+
+  secondRow: {
+    ':nth-child(2)': {
+      textAlign: 'left',
+    },
+  },
+
+  cellSpacing: {
+    padding: '15px',
+  },
+});
 
 export default CourseListRow;
