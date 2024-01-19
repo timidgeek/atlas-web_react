e4ca1b869b51ccdc4abccf49d836c7e23e06c226
timidgeek <9liveslinds@gmail.com>
2024-01-19 14:56:14 -0600

    updated header and login with aphrodite, got carried away with ok button

--- React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import logo from '../assets/atlas_logo.png';
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
 
 function Header() {
   return (
-    <header className="Header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>School dashboard</h1>
+    <header className={css(styles.header)}>
+      <img src={logo} className={css(styles.appLogo)} alt="Atlas Logo" />
+      <h1 className={css(styles.headerH1)}>School dashboard</h1>
     </header>
   )
 }
+
+// APHRODITE STYLES
+
+const styles = StyleSheet.create({
+  header: {
+    height: '15rem',
+    backgroundColor: '#FFFAE6',
+    display: 'flex',
+    alignItems: 'center',
+    color: '#001947'
+  },
+  
+  headerH1: {
+    display: 'inline',
+    fontFamily: 'Arial, Helvetica, sans-serif'
+  },
+  
+  appLogo: {
+    width: '20rem',
+    height: '10rem'
+  },
+})
 
 export default Header;
--- React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,21 +1,58 @@
 import React from 'react';
-import './Login.css';
+import { StyleSheet, css } from 'aphrodite';
+import { render } from 'enzyme';
 
 function Login() {
   return (
     <React.Fragment>
-      <div className="Login">
+      <div className={css(styles.loginMargin)}>
         <p>Login to access the full dashboard</p>
+    
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
-        
+        <input 
+          type="email" 
+          id="email" 
+          name="email"
+          className={css(styles.loginInputMargin)}
+          />
+
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+        <input 
+          type="password" 
+          id="password" 
+          name="password"
+          className={css(styles.loginInputMargin)}/>
         
-        <button>OK</button>
+        <button className={css(styles.okButton)}>OK</button>
       </div>
   </React.Fragment>
   )
 }
+
+// APHRODITE STYLES
+
+const styles = StyleSheet.create({
+  loginMargin: {
+    margin: '5rem'
+  },
+  
+  loginInputMargin: {
+    margin: '1rem'
+  },
+
+  okButton: {
+    borderRadius: '50%',
+    color: '#001947',
+    border: 'none',
+    fontSize: '15px',
+    backgroundColor: '#5FD3B0',
+    cursor: 'pointer',
+    padding: '5px 15px 5px 15px',
+    ':hover': {
+      color: 'white',
+    },
+  },
+
+})
 
 export default Login;
