3c14ba16a8b2c523478a90db7c440e42431d4269
timidgeek <9liveslinds@gmail.com>
2024-01-20 16:36:25 -0600

    i always forget to move progress to the next task's folder. still working on bounce

---
+++ React_inline_styling/task_4/dashboard/src/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from '../assets/atlas_logo.png';
+import { StyleSheet, css } from 'aphrodite';
+
+function Header() {
+  return (
+    <header className={css(styles.header)}>
+      <img src={logo} className={css(styles.appLogo)} alt="Atlas Logo" />
+      <h1 className={css(styles.headerH1)}>School dashboard</h1>
+    </header>
+  )
+}
+
+// APHRODITE STYLES
+
+const styles = StyleSheet.create({
+  header: {
+    height: '15rem',
+    backgroundColor: '#FFFAE6',
+    display: 'flex',
+    alignItems: 'center',
+    color: '#001947',
+    marginRight: '0',
+  },
+  
+  headerH1: {
+    display: 'inline',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+  },
+  
+  appLogo: {
+    maxWidth: '20rem',
+    maxHeight: '10rem',
+    '@media screen and (max-width: 900px)': {
+      maxWidth: '10rem',
+      maxHeight: '5rem',
+    }
+  },
+})
+
+export default Header;
