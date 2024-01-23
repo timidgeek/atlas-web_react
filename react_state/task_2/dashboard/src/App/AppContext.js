b10f09570db9028e2ff6f97b2004a2246befd0d9
timidgeek <9liveslinds@gmail.com>
2024-01-23 16:14:47 -0600

    app context created

--- react_state/task_2/dashboard/src/App/AppContext.js
+++ react_state/task_2/dashboard/src/App/AppContext.js
@@ -1,2 +1,16 @@
 import React from 'react';
 
+const user = {
+  email: '',
+  password: '',
+  isLoggedIn: false
+}
+
+const logOut = () => {}
+
+const AppContext = React.createContext({
+  user: user,
+  logOut: logOut
+})
+
+export default AppContext;
