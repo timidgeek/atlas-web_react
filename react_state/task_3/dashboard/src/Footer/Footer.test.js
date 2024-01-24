5a73a8da8777884bdbeeb0b182c94001e7a4a638
timidgeek <9liveslinds@gmail.com>
2024-01-24 12:13:33 -0600

    footer tests passing

--- react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Footer from './Footer';
 import { StyleSheetTestUtils } from 'aphrodite';
+import { AppContext } from '../App/AppContext';
 
 StyleSheetTestUtils.suppressStyleInjection();
 
@@ -13,5 +14,24 @@ describe('Footer Component', () => {
   it('renders the text "Copyright"', () => {
     const wrapper = shallow(<Footer />);
     expect(wrapper.text()).toContain('Copyright');
+  });
+
+  it('does not display the link when the user is logged out within the context', () => {
+    const wrapper = mount(
+      <AppContext.Provider value={{ user: { isLoggedIn: false } }}>
+        <Footer />
+      </AppContext.Provider>
+    );
+
+    expect(wrapper.find('.contact').exists()).toBe(false);
+  });
+
+  it('displays the link when the user is logged in within the context', () => {
+    const wrapper = mount(
+      <AppContext.Provider value={{ user: { isLoggedIn: true } }}>
+        <Footer />
+      </AppContext.Provider>
+    );
+    expect(wrapper.find('.contact').exists()).toBe(true);
   });
 });
